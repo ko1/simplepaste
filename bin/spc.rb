4ad94f7daa2e8fc75b5a7408a95344fdfdd00b8e
ko1 <ko1@atdot.net>
2011-06-15 14:34:14 +0900

    * fix -t option. * support proxy (by eban).

--- bin/spc.rb
+++ bin/spc.rb
@@ -10,25 +10,33 @@ require 'net/http'
 require 'optparse'
 Net::HTTP.version_1_2
 
-def paste str
-  body = CGI.escape(str)
+def paste title, body
   uri = URI.parse(TextPasteURL)
+  proxy = URI.parse(ENV["http_proxy"] || "")
 
-  Net::HTTP.start(uri.host, uri.port) {|http|
-    response = http.post(uri.path, 'paste_body=' + body)
+  post_body = "paste_body=#{CGI.escape(body)}"
+  if title
+    puts title
+    post_body += "&title=#{CGI.escape(title)}"
+  end
+
+  Net::HTTP.start(uri.host, uri.port, proxy.host, proxy.port) {|http|
+    response = http.post(uri.path, post_body)
     puts response.body
   }
 end
 
 OPTS={}
 opt = OptionParser.new
-opt.on("-t title"){|v| OPTS[:t] = v+"\n" }
+opt.on("-t [TITLE]"){|v|
+  OPTS[:title] = v
+}
 opt.parse!(ARGV)
 
 if ARGV.empty?
-  paste OPTS[:t]+ARGF.read
+  paste OPTS[:title], ARGF.read
 else
   ARGV.each{|file|
-    paste OPTS[:t]+File.read(file)
+    paste OPTS[:title], File.read(file)
   }
 end
